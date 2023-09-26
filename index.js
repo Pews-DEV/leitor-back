b25d8b01af9aed7a1387b4ca178c217790cbbecc
pews-dev <pedro.silvaifce@gmail.com>
2023-09-26 15:17:00 -0300

    feat: ajustes de cors

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 const plateRouter = require('./src/routers/plateRouter');
 require('dotenv').config();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
 app.use(express.json());
 app.use(plateRouter);
 
