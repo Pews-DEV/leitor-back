ea3254e4183e238b68b254c14fc441c443dda302
pews-dev <pedro.silvaifce@gmail.com>
2023-09-26 16:07:14 -0300

    fix: testando solucoes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const plateRouter = require('./routers/plateRouter');
-const app = express();
 
+const app = express();
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
-  
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(plateRouter);
 
 app.listen(8080);
