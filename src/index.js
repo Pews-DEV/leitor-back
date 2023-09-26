69c030f3a85fd2024ad0412557149a70ff0e7c09
pews-dev <pedro.silvaifce@gmail.com>
2023-09-26 16:09:42 -0300

    feat: adicionando cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const plateRouter = require('./routers/plateRouter');
 
 const app = express();
@@ -7,8 +8,10 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    app.use(cors());
     next();
   });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(plateRouter);
